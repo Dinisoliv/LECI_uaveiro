b0a095607689628c8496a6cfba63ec0417042dca
Dinis Oliveira <dinis.aoliveira@ua.pt>
2024-12-13 21:59:52 +0000

    more clean up

--- 1Ano/FP/Projeto2/Trabalho/teste.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def clear_powershell_console():
-    os.system('cls')  # for Windows
-
-# Call the function to clear the PowerShell console
-clear_powershell_console()
-
-print("Hello world")
--- 1Ano/FP/projeto fp_1/Versions/new 1().py
+++
@@ -1,7 +0,0 @@
-while True:
-    print("\n" + mostrar_forca(erros))
-    print(mostrar_palavra(palavra, letras_corretas))
-    print("Letras erradas:", ' '.join(sorted(letras_erradas)))
-    print("Número de erros:", erros)  # Mostra o número de erros
-    
-if __name__ == "__main__":
